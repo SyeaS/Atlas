314a4a0bb28e39adf67232cce85b3db288630b81
ProhaszkaAbel <prohaszka.abel@blathy.info>
2026-01-30 14:43:47 +0100

    Reinit frontend

--- Atlas.Frontend/atlas/src/app/app.ts
+++ Atlas.Frontend/atlas/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CalendarSimpleComponent } from './calendar-simple-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CalendarSimpleComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
